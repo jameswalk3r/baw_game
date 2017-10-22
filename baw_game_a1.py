7bb418f8b9ac8735a8d8701782a494fb534b2f07
Josh Briggs <imjbriggs01@gmail.com>
2017-10-22 18:08:42 -0500

    baw_game_a1.py

---
+++ baw_game_a1.py
@@ -0,0 +1,38 @@
+#Libraries to import
+import random
+import msvcrt as m
+
+#Functions
+def wait():
+    m.getch()
+
+
+
+
+#Core Ideas
+#Player selects Race and Class.
+#Class = How fast certain skills level up and slight skill boosts.
+
+#Skill levels 1-50; Race gives boosts to cetain skills.
+#Skills: Combat, Stealth, Magic, and Speech
+#Genetic Skills level by +10, class by +5, use by +1.]
+#Level Speed is for Non-Genetic Skills.
+
+#Races: Human(2), Elven(2), Half-Blood(1)
+#Human 1: Well-rounded; not best in any skills. Level speed slow.
+#Human 2: Low Stealth, high Speech. Level up speed medium.
+#Elven 1: Low Combat, high Magic. Level up speed medium/fast.
+#Elven 2: Low Combat, high Stealth. Level up speed medium.
+#Half-Blood: Low stealth, high magic. Level speed medium.
+
+
+
+
+#Start of our game
+print "Thank you for testing our game!"
+wait()
+print 'next part'
+
+
+
+
