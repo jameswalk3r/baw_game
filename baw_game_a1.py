35e4e7c56283185062a84dc5740eafa9af5e35bb
Josh Briggs <imjbriggs01@gmail.com>
2018-06-02 01:52:06 -0500

    Everything works aside from when you choose to explore other reigons and go right. It doesn't run the text I have under it, just skips to the end. I need some help.

--- baw_game_a1.py
+++ baw_game_a1.py
@@ -25,8 +25,9 @@ def print_and_wait(wait_text):
     getch()
 
 
+
 #WELCOME TO THE GAME
-print_and_wait('Welcome to the game! \nPress any key to continue...') # the \n is a way to start a new line in terminal when writing a terminal message
+print_and_wait('Welcome to The Crystal Key! \nPress any key to continue...\n') # the \n is a way to start a new line in terminal when writing a terminal message
 
 #Core Ideas
 #Player creates character's name
@@ -44,8 +45,6 @@ for each_race in races:
     print(each_race, races[each_race])
 
 player_race = int(input())
-
-print ('You have selected ' + str(player_race))
 
 if player_race == 0:
     player_race = dice.roll(1,3) # What happens if we add more races?
@@ -71,23 +70,25 @@ print_and_wait('You have chosen to be ' + races[player_race])
 #Half-Blood: Low stealth, high magic. Level speed medium.
 
 #Player selects class.
-print('What is your character\'s class?\n')
-c = int(input('Press 1 for Warrior, 2 for Mage, or 3 for Assassin.\n'))
-if c == 1:
-    print('Warrior class selected')
-elif c == 2:
-    print('Mage class selected')
-elif c == 3:
-    print('Assassin class selected')
+print('\nWhat is your character\'s class?\n')
+player_class = int(input('Press 1 for Warrior, 2 for Mage, or 3 for Assassin.\n'))
+if player_class == 1:
+    print('Warrior class selected\n\n')
+elif player_class == 2:
+    print('Mage class selected\n\n')
+elif player_class == 3:
+    print('Assassin class selected\n\n')
 
 
 
+# player_name = 'TESTNAME'
+# player_race = 1
+# player_class = 1
 
 #Start of our adventure
 
-print (player_name + '... Welcome to The Crystal Key!\n' + '\n')
-print ('''You have found yourself in dank musky dungeon deep under the ancient ruins Kalkavar, \
-with no memory of how you got there...  You realize you're at an intersection. \n''')
+print ('''You have found yourself in dank musky dungeon deep under the ancient ruins of Kalkavar, \
+with no memory of how you got there...\n  You realize you're at an intersection. \n''')
 player_move = int(input ('Which way do you want to go?\n\n 1.) Press Forward\n 2.) Turn Back\n 3.) Explore Other Reigons of the Ruins\n'))
 if player_move == 1:
     print ('You have decided to press forward.')
@@ -97,9 +98,18 @@ elif player_move == 3:
     print ('You have decided to explore the ruins further\n')
     explore_choice = input ('Which way do you want to go?\n Left or Right?\n')
     if str.lower(explore_choice) ==  'left':
-        print ('something')
-    elif str.lower(explore_choice) ==  'right':
-        print ('something else')
+        player_1L_choice = input ('There is a steep drop, it\'s safe to go down but you won\'t be able to get back up.\n\n 1.)Drop Down\n 2.)Turn Back\n')
+        if player_1L_choice == 1:
+            print ('drop placeholder')
+        if player_1L_choice == 2:
+            print ('back placeholder')
+        elif str.lower(explore_choice) ==  'right':
+            player_1L_choice = int(input ('You look down and see a tunnel with a chest at the bottom.\n\n 1.)Drop Down\n 2.)Turn Back\n'))
+    if player_1L_choice == 1:
+        print ('drop placeholder')
+    if player_1L_choice == 2:
+        print ('back placeholder')
+
 
 
 
--- test.py
+++ test.py
@@ -52,5 +52,20 @@ def print_and_wait(wait_text):
 #         print ('howdy')
 # print ('bye')
 
-if '1' == 1:
-    print('yay')
+print ('''Please type the number for your chosen race. 
+If you would like to pick a random race, select 0. \n''')
+ 
+races = { 1 : 'Human', 2 : 'Elven', 3 : 'Half-Blood'}
+for each_race in races:
+    print(each_race, races[each_race])
+
+player_race = int(input())
+
+if player_race == 0:
+    player_race = dice.roll(1,3) # What happens if we add more races?
+elif player_race > 3: #This is NOT going to work long term. What happens if they type a alpha?
+    # What happens if we add more races?
+    print('Please make a valid choice.')
+    player_race = int(input())
+
+print_and_wait('You have chosen to be ' + races[player_race])
